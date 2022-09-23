0af8218ce25fd24cfca8f43a3f98d72d266d5679
Warren Acosta <infinitycode001@gmail.com>
2022-09-22 20:26:48 -0400

    pratices folder

---
+++ frontend-react-rute/introduccion-a-la-programacion/practices/herencias/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Coche coche = new Coche();
+coche.maquina();
+
+interface Vehiculo
+{   
+    public void setVelocidadMaxima(double velocidadMaxima);
+    public double getVelocidadMaxima();
+}
+
+class Maquina
+{
+    public void maquina()
+    {
+        System.Console.WriteLine("Esta maquina tiene motor");
+    }
+}
+
+class Coche : Maquina, Vehiculo
+{   
+    double velocidadMaxima;
+    public void setVelocidadMaxima(double velocidadMaxima)
+    {
+        this.velocidadMaxima = velocidadMaxima;
+    }
+    public double getVelocidadMaxima()
+    {
+        return velocidadMaxima;
+    }
+}

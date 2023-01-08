2dc9a8acf064662e922c53e2b9ecc623ee82e4f4
Warren Acosta <infinitycode001@gmail.com>
2023-01-08 11:34:56 -0400

    exercise lesson 5 finished

--- frontend-react-rute/javascript-basico/05-Numbers/convertions.js
+++ frontend-react-rute/javascript-basico/05-Numbers/convertions.js
@@ -1,5 +1,58 @@
 // ! Operador .valueOf() - Obtener valores numericos a partir de literales
-// ! NaN - Infinity
-// ! Numero hexadecimales
+
+let a = 2;
+let b = new Number(3);
+
+console.log(a);
+console.log(b);
+console.log(a + b);
+console.log(a.valueOf() + b.valueOf());
+
+console.log(b.valueOf());
+
+let str = new String("Hola soy un string");
+console.log(str);
+console.log(str.valueOf());
+
+// ! NaN (Not a Number) - Infinity
+
+let n = Number("Adios");
+console.log(n);
+console.log(isNaN(n));
+
+let num = 19;
+let div = 0;
+
+console.log(num / div);
+
 // ! Convertir los string a valores numericos con Number, parseInt y parseFloat
+
+let numero = '5.89';
+let num2 = 17.2;
+
+console.log(numero + num2); // Error de concept
+
+console.log(Number(numero) + num2);
+
+console.log(parseInt(numero));
+console.log(parseFloat(numero));
+
+let num3 = 3;
+console.log(parseFloat(num3));
+
+// ! Numero hexadecimales (base 16)
+
+let numHex = '0x3a5b7';
+
+console.log(parseInt(numHex, 16));
+
 // ! Obtener los valores maximo y minimo en JavaScript
+
+let min_precision = Number.EPSILON;
+let min_valor_JS = Number.MIN_VALUE;
+let max_valor_JS = Number.MAX_VALUE;
+
+console.log(min_precision);
+console.log(min_valor_JS);
+console.log(max_valor_JS);
+console.log(2 ** 1023);
---
+++ frontend-react-rute/javascript-basico/05-Numbers/exercise/exercise.js
@@ -0,0 +1,8 @@
+let altura_cm = 174;
+let altura_m = 1.74;
+let libra_kg = 64.41;
+let altura_m_redondeada_arriba = altura_m.toFixed()
+let libra_kg_redondeada_abajo = libra_kg.toPrecision(2)
+
+let bool = (Number.MAX_VALUE + 1) === (Number.MAX_VALUE); // true
+

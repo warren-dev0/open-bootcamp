8a15fdfb41dad1f7741995c0b18a35f49cc360df
Warren Acosta <infinitycode001@gmail.com>
2023-04-03 12:34:33 -0400

    new route c#

---
+++ backend-c#/c#/Introduccion/exercise/ejercicio1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Escribe tu nombre");
+string name = Console.ReadLine();
+
+Console.WriteLine("Tu nombre es: " + name);
---
+++ backend-c#/c#/Introduccion/exercise/ejercicio2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Digite la hora");
+string hour = Console.ReadLine();
+
+Console.WriteLine("La hora es: " + hour);
